a88febd801f9d5c133a79c09184250d423161b2a
Benn Walsh <7524135+bennwalsh@users.noreply.github.com>
2024-07-17 14:08:08 +0100

    completed exerise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    const strRev = str.split('').reverse().join('');
+    return strRev;
 };
 
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
