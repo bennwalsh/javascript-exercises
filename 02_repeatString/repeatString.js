5fb19c430bf1cc218a38ce16537956839a448197
Benn Walsh <7524135+bennwalsh@users.noreply.github.com>
2024-07-17 13:38:56 +0100

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let finalString = ""
+    for (let i = 1; i <= num; i++)
+        finalString += string;
+    return finalString;
 };
+console.log(repeatString("Hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
